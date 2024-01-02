df9899fd920c1c04a1203609d0dc8e680dd40afd
Ikhlas Bhojani <ikhlasbhojani00@gmail.com>
2024-01-02 20:53:49 +0500

    .

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Tecno World</h1>
+        <p>@The Name Of New World</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/channel/UCP8R_5OsHen1geJ4uS-UdkA" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/ikhlasbhojani" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
